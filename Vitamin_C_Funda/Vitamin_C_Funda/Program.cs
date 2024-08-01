8b070399b6bdb7aaf0dfa379a39974dc6f98ffd0
sangth <thaisangcr7@gmail.com>
2024-08-01 16:44:09 -0400

    commit new folder for C# Fundamental with Pluralsight

---
+++ Vitamin_C_Funda/Vitamin_C_Funda/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+
+namespace Vitamin_C_Funda;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("Please enter your name: ");
+
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello " + name);
+    }
+}
