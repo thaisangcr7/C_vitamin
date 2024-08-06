aa9f7f941b38dc9d09dd941713ccbf10a05abd35
sangth <thaisangcr7@gmail.com>
2024-08-06 02:28:30 -0400

    Sang

--- CSharp_Mini_8hrs/CSharp_Mini_8hrs/10_Console_InputOutput.cs
+++ CSharp_Mini_8hrs/CSharp_Mini_8hrs/10_Console_InputOutput.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Runtime.Intrinsics.Arm;
+using System.Transactions;
 
 namespace CSharp_Mini_8hrs
     
@@ -15,7 +17,20 @@ namespace CSharp_Mini_8hrs
             System.Console.WriteLine("Hey hey Today is Aug 6, 2:06 AM " + name);
             */
             // write on the same line
-            System.Console.Write("Enter your name here: ");
+            /*System.Console.Write("Enter your name here: ");
+
+            System.Console.Write("Enter your age: ");
+            string age = Console.ReadLine();
+            System.Console.WriteLine(age);
+            */
+            Console.Write("Enter your name here: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Enter your name here: " + name);
+            
+            Console.Write("Enter your age: ");
+            var age = Console.ReadLine();
+            Console.WriteLine("Enter your age: " + age);
+            
 
         }
             
