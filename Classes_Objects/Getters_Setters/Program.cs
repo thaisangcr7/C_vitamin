94938ee6092b0902c9d122c2ed7f5b0bf503ebf3
sangth <thaisangcr7@gmail.com>
2024-08-23 02:03:41 -0400

    s

---
+++ Classes_Objects/Getters_Setters/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Getters_Setters;
+
+class Program
+{
+    static void Main(string[] args)
+
+    {
+        // Getters_Setters are different types of methods that sure the classes are secure 
+        Movie avengers = new Movie("The Avengers", "Joss Whedon", "R");
+        Movie Shrek = new Movie("Shrek", "Adam", "PG");
+
+        // valid rating we can have for rating is below
+        // G, PG, PG-13, R, NR
+        
+        //Console.WriteLine(avengers.rating) // this will give the error since rating is private inside the Movie class
+        Console.WriteLine(avengers.Rating);
+        Console.ReadLine();
+         
+    }
+}
