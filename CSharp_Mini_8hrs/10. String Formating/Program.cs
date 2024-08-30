a63c987808cca28d815b0b02464bb121617563da
sangth <thaisangcr7@gmail.com>
2024-08-30 00:20:03 -0400

    String Formating

---
+++ CSharp_Mini_8hrs/10. String Formating/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _10._String_Formating;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = "Aba";
+        int age = 25;
+
+        System.Console.WriteLine("Name " + name);
+        System.Console.WriteLine("Age " + age);
+
+        System.Console.WriteLine();
+
+        System.Console.WriteLine("Name: " + name + "\nAge: " + age);
+
+        System.Console.WriteLine(" Your name is " + name + " , and your age is " + age);
+
+        // Better Formatting
+        System.Console.WriteLine(" Your name is {0}, and your age is {1}", name, age); // better formating     
+
+        System.Console.WriteLine("Name: {0}\nAge: {1}", name, age);
+
+    }
+}
