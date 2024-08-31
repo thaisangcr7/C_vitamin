d236dd841fad1aa9bf4f28e16d5f1d41de2116e4
sangth <thaisangcr7@gmail.com>
2024-08-30 23:56:58 -0400

    IsNullorEmpty

---
+++ CSharp_Mini_8hrs/14. String is Null or Empty Function/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _14._String_is_Null_or_Empty_Function;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine($" Your name is {name}");
+
+        //
+        // Check if the string is null or empty
+            // if (name != "")
+            //      System.Console.WriteLine("0");  
+
+            // if (!name.Equals(""))
+                // System.Console.WriteLine("1");
+
+
+        // if the string is null or empty then print 2 
+            if(!string.IsNullOrEmpty(name))
+            System.Console.WriteLine("2");
+            
+ 
+        Console.ReadLine();  
+    }
+}
