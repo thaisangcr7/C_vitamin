159c498ade67e957ce5348cd0723b0c5f0f557cb
sangth <thaisangcr7@gmail.com>
2024-09-10 23:58:41 -0400

    5:52 - ex sum int array

--- CSharp_Mini_8hrs/34. Exercise _ Sum of Int Array/Program.cs
+++ CSharp_Mini_8hrs/34. Exercise _ Sum of Int Array/Program.cs
@@ -1,9 +1,43 @@
-﻿namespace _34._Exercise___Sum_of_Int_Array;
+﻿using System.Globalization;
+
+namespace _34._Exercise___Sum_of_Int_Array;
 
 class Program
 {
+    // Create and initialise int array of numbers
+    // Create function SumofNumbers with int retun type
+    // Int array param
+    // Funcrion should retun total of all numbers
+    // Call in main and output the total
+        // Extra: Check array length
+        // return -1 if array empty
+        // check return in main and output message
+        // do we need to retun -1, how else can we make this?
     static void Main(string[] args)
     {
+        //1.  Create and intialise array
+        int[] numbers = new int[]
+        {
+            0,1,2,3,4,5
+        };
+
+        // 3. Call the number array
+        System.Console.WriteLine(SumofNumbers(numbers));
         
     }
+
+        // 2. Create function - SumofNumbers to call an array
+        static int SumofNumbers(int[] numbers)
+        {
+            int total = 0;
+
+            // create a for loop - to go through an array
+            foreach (int item in numbers)
+            {
+                total += item;
+            }
+
+            return total;
+
+        }
 }
--- CSharp_Mini_8hrs/34. Exercise _ Sum of Int Array/obj/Debug/net8.0/34. Exercise _ Sum of Int Array.AssemblyInfo.cs
+++ CSharp_Mini_8hrs/34. Exercise _ Sum of Int Array/obj/Debug/net8.0/34. Exercise _ Sum of Int Array.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("34. Exercise _ Sum of Int Array")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5bd16d0d146b94826cdafe0e81c074bf65c85fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+883f40fad4873ff73ba3401acd74c9426d9338d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("34. Exercise _ Sum of Int Array")]
 [assembly: System.Reflection.AssemblyTitleAttribute("34. Exercise _ Sum of Int Array")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
