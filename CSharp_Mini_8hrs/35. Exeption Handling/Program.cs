5a34f419f529816c561719aea5d6c53b1e967e3a
sangth <thaisangcr7@gmail.com>
2024-09-12 00:23:55 -0400

    Handling exception

---
+++ CSharp_Mini_8hrs/35. Exeption Handling/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _35._Exeption_Handling;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // how to handle exception when user put in wrong inputs
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        System.Console.WriteLine(number);
+
+        Console.ReadLine();
+    }
+}
---
+++ CSharp_Mini_8hrs/36. Try Catch _ Handle Exception/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _36._Try_Catch___Handle_Exception;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        // System.FormatException
+        // System.OverflowException
+        
+        // Declare a boolean variable to control the while loop
+        bool looping = true;
+       
+       // Start an infinite loop that will continue until the looping variable is set to false
+        while (looping)
+        {
+            try
+            {
+                Console.Write("Enter a number: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                System.Console.WriteLine(number);
+                
+                // Set the looping variable to false to exit the loop
+                looping = false;
+
+                
+            }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number."); 
+                }
+                catch (System.OverflowException)
+                {
+                    Console.WriteLine("Please Enter an number that less than 2 billion");
+                }
+                catch (System.Exception)
+                {
+                    System.Console.WriteLine("Something went wrong");
+                } 
+
+        }
+
+        // Print a goodbye message after the loop has finished
+        System.Console.WriteLine("Good Bye!");
+
+
+        Console.ReadLine();
+    }
+}
