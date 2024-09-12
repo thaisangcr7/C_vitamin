70b0860ddef0814939b50f27f6dd5d57ba9de028
sangth <thaisangcr7@gmail.com>
2024-09-12 00:41:08 -0400

    S

---
+++ CSharp_Mini_8hrs/37. Printing Error Messages/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _37._Printing_Error_Messages;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.Write("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num);
+        }
+        catch (System.Exception e)
+        {
+            
+            System.Console.WriteLine($"error: {e.Message}");
+        }
+    }
+}
