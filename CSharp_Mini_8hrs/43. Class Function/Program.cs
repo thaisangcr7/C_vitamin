075bda49c1ee9ad09e1a975100140c574a545d47
sangth <thaisangcr7@gmail.com>
2024-09-13 22:35:23 -0400

    class function

---
+++ CSharp_Mini_8hrs/43. Class Function/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace _43._Class_Function;
+
+class Program
+{
+
+    class Person
+    {
+        public string name;
+        public int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+
+        }
+
+        // function inside the class
+        public string ReturnDetails()
+        {
+            return $"Name: {name}\nAge: {age}";
+        }
+    }
+    static void Main(string[] args)
+    {
+        Person person = new Person("sang", 19);
+            //System.Console.WriteLine($"Name: {person.name}\nAge: {person.age}");
+            // using function - just for reference because it is not scable
+                //System.Console.WriteLine(ReturnDetails(person.name, person.age));
+                //System.Console.WriteLine(ReturnDetails(person));
+
+        // call function that was wrapped inside the class
+        System.Console.WriteLine(person.ReturnDetails());
+    }
+
+    // function
+    // static string ReturnDetails(string name, int age)
+    // {
+    //     return $"Name: {name}\nAge: {age}";
+    // }
+
+    // function is to pass the class in
+        // static string ReturnDetails(Person person)
+        // {
+        //     return $"Name: {person.name}\nAge: {person.age}";
+
+        // }
+}
