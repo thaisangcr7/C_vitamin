62c7083b5086af6899bfae5d9ebb2d26ea619515
sangth <thaisangcr7@gmail.com>
2024-09-21 14:36:47 -0400

    Comments Class - One to many relationship

--- FinSharp/api/Models/Comment.cs
+++ FinSharp/api/Models/Comment.cs
@@ -1,8 +1,25 @@
 using System;
 
-namespace api.Models;
-
-public class Comment
+namespace api.Models
 {
+
+    public class Comment
+    {
+        // link the comment with stock with Convention 
+        // to form the relationship
+        public int ID { get; set; }
+        public string Title { get; set; } = string.Empty; // title for comments
+        public string Content { get; set; } = string.Empty;
+
+        public DateTime  CreateOn { get; set; } = DateTime.Now;
+
+        public int? StockId { get; set; } // key
+        // Navigation
+        // Navigate within our model and relationship
+        public Stocks? Stocks { get; set;} // this is the navigation property allow us to access the properties
+ 
+
+    }
+
 
 }
