b0dfef5fb7548b79686155b45cc5a972022854a2
sangth <thaisangcr7@gmail.com>
2025-03-08 16:00:54 -0500

    practice for fun

--- Practice_C#/ConsoleApp1/Program.cs
+++ Practice_C#/ConsoleApp1/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+
+namespace Helloworld
+{
+    class Program
+    {
+        // this is the main method
+        static void Main(string[] args)
+        {
+            WriteSomething();
+            int result = Calculation();
+            Console.WriteLine($"result is {result}");
+            Console.ReadLine();
+        }
+
+        // Access modifier (static) return method name (parameter1, parameter2)
+        // Why static - whenever you want to call a method within a static method, you need to
+        // make the method itself static as well        
+        public static void WriteSomething()
+        {
+            System.Console.WriteLine(" i am called from the method");
+            Console.ReadLine();
+        }
+
+        public static int Calculation()
+        {
+            // int num1 = 10;
+            // int num2 = 10;
+            // int result = num1 + num2;
+            // return result;
+
+            //ask for user inputs
+            Console.Write("Please enter your your num1: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter your num2: ");
+            int num2 = int.Parse(Console.ReadLine());
+            int result = num1 + num2;
+            return result;
+        }
+    }
+}
+
+
+
--- Practice_C#/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ Practice_C#/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+286f72311a1b87f7981c0145d2a0d1a07f33b95b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a540edcba87cfaf07cd6ca56588c6607e66d6097")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
